7d5d299a98ab9883403d227bd7951bfa6a234250
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-08-01 19:54:31 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "687d3da913843567b07fe0f6820a73d4",
   base: "https://api.openweathermap.org/data/2.5/",
 };
